1cb2979a85eaada33b8419506db47501d4162b62
jaebin <jaebin@wemeetplace.com>
2021-12-30 15:43:09 +0900

    #5 pr 수정

--- app/src/main/java/com/example/ktrotest/util/DateSetter.kt
+++ app/src/main/java/com/example/ktrotest/util/DateSetter.kt
@@ -17,8 +17,9 @@ object DateSetter {
     fun getYear(): Int = nowDate.year
     fun getMonth(): Int = nowDate.monthValue
     */
+    @JvmStatic
     val nowDate = Date(System.currentTimeMillis())
-
+    @JvmStatic
     val yesterDay = Date(System.currentTimeMillis()-86400000)
 
 
@@ -31,8 +32,4 @@ object DateSetter {
     fun <T>getDate(time: T): Int = date.format(time).toInt()
 
     //하루 전 날짜 -> 해당 당일 박스오피스 정보는 없음..
-
-
-
-
 }
